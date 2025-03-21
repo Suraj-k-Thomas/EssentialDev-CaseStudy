ad764e53693a1bd0c74b00626ae0cb589d4c5343
surajkthomas <ksurajt@gmail.com>
2025-03-21 17:08:19 +0530

    FeedItem modal created

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Suraj  Thomas on 21/03/25.
+//
+
+import Foundation
+
+struct  FeedItem {
+    let id: String
+    let description: String?
+    let imageURL: URL
+    let location: String?
+}
