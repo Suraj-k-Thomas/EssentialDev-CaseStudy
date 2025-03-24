2fe667090d50092545b128c20b8eda3cf1c93d42
surajkthomas <ksurajt@gmail.com>
2025-03-24 16:43:40 +0530

    refactoring RemoteFeedLoader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,15 +11,17 @@ import XCTest
 class RemoteFeedLoader {
     
     let client : HTTPClient?
-    let url : URL?
-    init (client : HTTPClient, url : URL?){
-        
+    let url : URL
+   
+    init (client : HTTPClient, url : URL){
+       
         self.client = client
         self.url = url
     }
+   
     func load (){
         
-        client!.get(fromUrl :URL(string: "https://TestUrl.com")!)
+        client!.get(fromUrl :url)
         
     }
 }
@@ -30,20 +32,16 @@ protocol HTTPClient {
     
 }
 
-class HTTPClientSpy : HTTPClient {
-    
-    var requestedUrl: URL?
-     func get (fromUrl url: URL) {
-        requestedUrl = url
-    }
-}
+
 
 class RemoteFeedLoaderTests : XCTestCase {
     
     func test_init_DoesNotREqruireDataFromURL () {
+       
         let url = URL(string: "https://TestUrl.com")!
         let (_,Client) = makeSUTAndCient(url: url)
         XCTAssertNil(Client.requestedUrl)
+        
     }
     
     func test_LoadRequestDataFromURL () {
@@ -51,21 +49,25 @@ class RemoteFeedLoaderTests : XCTestCase {
         let url = URL(string: "https://TestUrl.com")!
         let (SUT,Client) = makeSUTAndCient(url: url)
         
-       // RemoteFeedLoader(client:Client ,url: url)
         SUT.load()
         XCTAssertEqual(url,Client.requestedUrl)
         
     }
-    
-    //Helpers
-    func makeSUTAndCient (url:URL) -> (SUT : RemoteFeedLoader, client : HTTPClientSpy){
-        
-        let client = HTTPClientSpy ()
+}
 
-        let SUT = RemoteFeedLoader(client: client, url: url)
-        return (SUT:SUT,client:client)
-        
-    }
-     
+//Helpers
+func makeSUTAndCient (url:URL) -> (SUT : RemoteFeedLoader, client : HTTPClientSpy){
     
+    let client = HTTPClientSpy ()
+    let SUT = RemoteFeedLoader(client: client, url: url)
+    return (SUT:SUT,client:client)
+    
+}
+
+class HTTPClientSpy : HTTPClient {
+    
+    var requestedUrl: URL?
+    func get (fromUrl url: URL) {
+        requestedUrl = url
+    }
 }
