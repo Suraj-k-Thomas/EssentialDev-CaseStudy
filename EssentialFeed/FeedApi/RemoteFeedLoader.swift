aff6d8035718d7b2600b442959192f5798ab1e1b
surajkthomas <ksurajt@gmail.com>
2025-04-17 16:19:36 +0530

    Replace 'RemoteFeedLoader.Result' enum with predefined `LoadFeedResult`  enum from the feed module (in preparation to confirm to `FeedLoader` protocol )

--- EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -11,21 +11,17 @@ import Foundation
 
 public final class RemoteFeedLoader {
     
+    private let client : HTTPClient?
+    private let url : URL
+    
     public enum Error : Swift.Error {
         
         case Connectivity
         case invalidData
     }
     
-    public enum Result : Equatable {
-        
-        case success([FeedItem])
-        case failure(Error)
-    }
-    
-
-    private let client : HTTPClient?
-    private let url : URL
+    public typealias Result = LoadFeedResult<Error>
+//
     
     public  init (client : HTTPClient, url : URL){
         
@@ -45,7 +41,7 @@ public final class RemoteFeedLoader {
                 completion(FeedItemsMapper.map(data, from: response))
                 
             case .failure:
-                completion(.failure(.Connectivity))
+                completion(.failure(Error.Connectivity))
             case .none:
                 print("nil value")
             }
--- EssentialFeed/FeedItems/FeedLoader.swift
+++ EssentialFeed/FeedItems/FeedLoader.swift
@@ -7,13 +7,16 @@
 
 import Foundation
 
-enum LoadFeedResult {
+public enum LoadFeedResult <Error: Swift.Error>{
     
     case success([FeedItem])
     case failure(Error)
 }
+
+extension LoadFeedResult:Equatable where Error:Equatable{}
+
 // protocol feedloader
 protocol FeedLoader {
-    
-    func loadFeed(completion:@escaping([LoadFeedResult])->Void)
+    associatedtype Error: Swift.Error
+    func loadFeed(completion:@escaping([LoadFeedResult<Error>])->Void)
 }
