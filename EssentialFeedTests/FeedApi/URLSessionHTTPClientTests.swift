af39e7dbd64a4171e1d90ca9043b51811a294166
surajkthomas <ksurajt@gmail.com>
2025-05-15 16:14:23 +0530

    Add nil data and nil response to the request error test case to make the setup more explicit

--- EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
@@ -36,7 +36,7 @@ class URLSessionHTTPClientTests :XCTestCase{
         let sut = URLSessionHTTPClient()
         let exp = expectation(description: "completion handler invoked")
         let error = NSError(domain: "some error", code: 1)
-        URLProtocolstub.stub(url: url, error: error)
+        URLProtocolstub.stub(url: url, data : nil , response : nil ,  error: error)
         sut.get(from: url){result in
             switch result
             {
@@ -56,12 +56,15 @@ class URLSessionHTTPClientTests :XCTestCase{
         static private var stubs = [URL:Stub]()
         
         private struct Stub {
+            let data : Data?
+            let response : URLResponse?
             let error : Error?
+          
         }
         
         
-        static  func stub(url: URL,error:Error? = nil) {
-            stubs[url] = Stub( error: error)
+        static  func stub(url: URL,data: Data?,response : URLResponse? ,error:Error?) {
+            stubs[url] = Stub(data: data, response: response ,error:error)
         }
         
         static func startInterceptingRequests () {
@@ -87,6 +90,14 @@ class URLSessionHTTPClientTests :XCTestCase{
         override func startLoading() {
             
             guard let url = request.url , let stub = URLProtocolstub.stubs[url] else {return}
+            
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
             
             if let error = stub.error {
                 client?.urlProtocol(self,didFailWithError : error)
